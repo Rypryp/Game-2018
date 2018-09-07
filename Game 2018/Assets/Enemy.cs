b3669c9fd4960678598afac8e894f183d86e1176
DESKTOP-UT6ATB1\Pyry <vuorelapyry@gmail.com>
2018-09-07 16:38:49 +0300

    Character movement part 2

---
+++ Game 2018/Assets/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int Health;
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+
+        if(Health <= 0)
+        {
+            OnDeath();
+        }
+    }
+
+    private void OnDeath()
+    {
+        Destroy(gameObject);
+    }
+}
--- Game 2018/Assets/Player.cs
+++ Game 2018/Assets/Player.cs
@@ -2,16 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class Player : MonoBehaviour {
 
     public float Speed = 5f;
     public float LookSensitivityX;
     public float LookSensitivityY;
 
     public Camera PlayerCamera;
-
     private Rigidbody rigidbody;
-
 
     void Start()
     {
@@ -20,23 +18,23 @@ public class PlayerMovement : MonoBehaviour {
 
     void Update()
     {
-        Vector3 bodyRotation = new Vector3(0, Input.GetAxisRaw("Mouse X"), 0) * LookSensitivityX;
-        transform.localEulerAngles += bodyRotation;
-
-
-        Vector3 cameraRotation = new Vector3(Input.GetAxisRaw("Mouse Y") * -1, 0, 0) * LookSensitivityY;
-
-        print((PlayerCamera.transform.localEulerAngles.x + cameraRotation.x));
-
-        PlayerCamera.transform.localEulerAngles += cameraRotation;
-
-      /*  if (PlayerCamera.transform.localEulerAngles.x + cameraRotation.x <= 90 && PlayerCamera.transform.localEulerAngles.x + cameraRotation.x >= -90)
+        PlayerRotation();
+        
+        if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-        }*/
+            Fire();
+        }
     }
 
 
     void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+
+
+    private void PlayerMovement()
     {
         if (Input.GetKey(KeyCode.W))
         {
@@ -61,5 +59,33 @@ public class PlayerMovement : MonoBehaviour {
             Vector3 movement = rigidbody.position + PlayerCamera.transform.transform.right * -1 * Speed * Time.fixedDeltaTime;
             rigidbody.MovePosition(movement);
         }
+    }
+
+    private void PlayerRotation()
+    {
+
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+
+
+       /* Vector3 bodyRotation = new Vector3(0, Input.GetAxisRaw("Mouse X"), 0) * LookSensitivityX;
+        transform.localEulerAngles += bodyRotation;
+
+        Vector3 cameraRotation = new Vector3(Input.GetAxisRaw("Mouse Y") * -1, 0, 0) * LookSensitivityY;
+
+        PlayerCamera.transform.localEulerAngles += cameraRotation;
+        PlayerCamera.transform.localEulerAngles = new Vector3(PlayerCamera.transform.localEulerAngles.x, PlayerCamera.transform.localEulerAngles.y, 0);
+
+        PlayerCamera.transform.Rotate()*/
+    }
+
+    private void Fire()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.forward,out hit))
+        {
+            hit.collider.gameObject.SendMessage("TakeDamage", 10, SendMessageOptions.DontRequireReceiver);
+        }        
     }
 }
